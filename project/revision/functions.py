f2c79c4755a0e0235a97a59070c0c21447bf7328
Your Name <you@example.com>
2025-09-17 12:48:42 +0300

    song

---
+++ project/revision/functions.py
@@ -0,0 +1,2 @@
+def is_number_bigger_than_given(candidate_number: float, threshold: float) -> bool:
+    return candidate_number > threshold
---
+++ project/revision/main.py
@@ -0,0 +1,9 @@
+from functions import  is_number_bigger_than_given
+
+def main():
+    result = is_number_bigger_than_given(candidate_number = 5, threshold=10)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
